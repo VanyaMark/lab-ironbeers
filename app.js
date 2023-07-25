0c5aaf96b578b60d5faedccf8f7a00e543157864
VanyaMark <markova.vanya@gmail.com>
2023-07-25 23:24:18 +0200

    Ironbeers express and handlebars reviewed

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + "/views/partials")
 
 // Add the route handlers here:
 
@@ -22,4 +22,35 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res, next) => {
+
+punkAPI
+    .getBeers()
+    .then(beersArr => {
+      console.log(beersArr)
+      res.render('beers', {beersArr})
+    })
+    .catch(error => console.log(error)) 
+})
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      console.log('Random Beer: ', randomBeer)
+      res.render('randomBeer', {randomBeer})
+    })
+})
+
+app.get('/beers/:id', (req, res, next) => {
+  console.log(req.params.id)
+  const paramsBeerId = req.params.id;
+
+  punkAPI
+    .getBeer(paramsBeerId)
+    .then(beer => {
+      res.render('beer', {beer})
+    })
+})
+
+app.listen(3005, () => console.log('🏃‍ on port 3005'));
